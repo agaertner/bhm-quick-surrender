6c8704185661635cf6178dcd3b6c1817dfcd1d82
GitHub Action <action@github.com>
2023-11-11 00:07:31 +0000

    (skip): 🔖 bump version to 0.2.5-dev.6

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -18,3 +18,6 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.2.5.6")]
+[assembly: AssemblyFileVersion("0.2.5.6")]
+[assembly: AssemblyInformationalVersion("0.2.5-dev.6+Branch.dev.Sha.5da880282c99b5133643ee5d95984c57d9e7496a")]
